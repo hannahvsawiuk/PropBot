90686875f84d42c45e6ae0021cc3dc7cd60bb942
Jack Guo <jackguo380@gmail.com>
2020-02-12 22:54:13 -0800

    Start testing realsense

--- vehicle_autonomy/propbot_slam/config/propbot.lua
+++ vehicle_autonomy/propbot_slam/config/propbot.lua
@@ -27,15 +27,15 @@ options = {
   use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 1,
+  num_laser_scans = 0,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 0,
+  num_point_clouds = 1,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.,
+  rangefinder_sampling_ratio = 0.25,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
@@ -47,7 +47,7 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
 TRAJECTORY_BUILDER_2D.min_range = 0.3
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 4.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
@@ -67,7 +67,7 @@ POSE_GRAPH.constraint_builder.min_score = 0.85 -- Increase
 POSE_GRAPH.global_constraint_search_after_n_seconds = 30 -- Increase
 
 ---------Global/Local SLAM---------
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100 -- Decrease
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10 -- Decrease
 TRAJECTORY_BUILDER_2D.max_range = 10. -- Decrease
 
 -------------------------------------------------------------------------------------
