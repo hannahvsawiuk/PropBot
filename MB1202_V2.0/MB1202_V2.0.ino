113b68c5900919930dc7257e3ea16568f4b24a1f
zhaoshengEE <43089911+zhaoshengEE@users.noreply.github.com>
2020-03-01 19:05:30 -0800

    Add files via upload

---
+++ MB1202_V2.0/MB1202_V2.0.ino
@@ -0,0 +1,87 @@
+#define SCL_PIN 5              //Default SDA is Pin5 PORTC for the UNO -- you can set this to any tristate pin
+#define SCL_PORT PORTC 
+#define SDA_PIN 4              //Default SCL is Pin4 PORTC for the UNO -- you can set this to any tristate pin
+#define SDA_PORT PORTC
+#define I2C_TIMEOUT 100        //Define a timeout of 100 ms -- do not wait for clock stretching longer than this time
+
+#include <SoftI2CMaster.h>     //You will need to install this library
+
+void setup(){
+  // Initialize both the serial and I2C bus
+  Serial.begin(9600);
+  i2c_init();
+}
+
+void loop()
+{
+  // Read a sensor at the default address
+  int distance = read_the_sensor_example();
+  if(distance == 25) {
+    Serial.print("Unsafe distance\n");
+     // Shut down the car
+  }
+
+}
+
+//////////////////////////////////////////////////////////
+// Code Example: Read the sensor at the default address //
+//////////////////////////////////////////////////////////
+int read_the_sensor_example(){
+  boolean error = 0;  //Create a bit to check for catch errors as needed.
+  int distance;
+  
+  //Take a range reading at the default address of 224
+  error = start_sensor(224);    //Start the sensor and collect any error codes.
+  if (!error){                  //If you had an error starting the sensor there is little point in reading it as you will get old data.
+    delay(100);
+    distance = read_sensor(224);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+    //Serial.print("Distance:");Serial.println(range);Serial.print("cm");
+    Serial.println((String)"Distance:"+distance+" cm");
+    return distance;
+  }
+}
+
+///////////////////////////////////////////////////
+// Function: Start a range reading on the sensor //
+///////////////////////////////////////////////////
+//Uses the I2C library to start a sensor at the given address
+//Collects and reports an error bit where: 1 = there was an error or 0 = there was no error.
+//INPUTS: byte bit8address = the address of the sensor that we want to command a range reading
+//OUPUTS: bit  errorlevel = reports if the function was successful in taking a range reading: 1 = the function
+//  had an error, 0 = the function was successful
+boolean start_sensor(byte bit8address){
+  boolean errorlevel = 0;
+  bit8address = bit8address & B11111110;               //Do a bitwise 'and' operation to force the last bit to be zero -- we are writing to the address.
+  errorlevel = !i2c_start(bit8address) | errorlevel;   //Run i2c_start(address) while doing so, collect any errors where 1 = there was an error.
+  errorlevel = !i2c_write(81) | errorlevel;            //Send the 'take range reading' command. (notice how the library has error = 0 so I had to use "!" (not) to invert the error)
+  i2c_stop();
+  return errorlevel;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////
+// Function: Read the range from the sensor at the specified address //
+///////////////////////////////////////////////////////////////////////
+//Uses the I2C library to read a sensor at the given address
+//Collects errors and reports an invalid range of "0" if there was a problem.
+//INPUTS: byte  bit8address = the address of the sensor to read from
+//OUPUTS: int   range = the distance in cm that the sensor reported; if "0" there was a communication error
+int read_sensor(byte bit8address){
+  boolean errorlevel = 0;
+  int range = 0;
+  byte range_highbyte = 0;
+  byte range_lowbyte = 0;
+  bit8address = bit8address | B00000001;  //Do a bitwise 'or' operation to force the last bit to be 'one' -- we are reading from the address.
+  errorlevel = !i2c_start(bit8address) | errorlevel;
+  range_highbyte = i2c_read(0);           //Read a byte and send an ACK (acknowledge)
+  range_lowbyte  = i2c_read(1);           //Read a byte and send a NACK to terminate the transmission
+  i2c_stop();
+  range = (range_highbyte * 256) + range_lowbyte;  //compile the range integer from the two bytes received.
+  if(errorlevel){
+    return 0;
+  }
+  else{
+    return range;
+  }
+}
